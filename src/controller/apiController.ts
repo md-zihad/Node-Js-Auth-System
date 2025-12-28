0881472756f75c44ed97c50cf9a420b2dc6e6c96
Md. Zihad <md.zihad0246@gmail.com>
2025-12-29 04:23:50 +0600

    feat: Adds input validation to user registration endpoint
    Introduces joi schema-based validation for registration requests

--- src/controller/apiController.ts
+++ src/controller/apiController.ts
@@ -3,6 +3,12 @@ import httpResponse from '../util/httpResponse'
 import responseMessage from '../constant/responseMessage'
 import httpError from '../util/httpError'
 import quicker from '../util/quicker'
+import { IRegisterRequestBody } from '../types/userTypes'
+import { validateJoiSchema, validateRegisterBody } from '../service/validationService'
+
+interface IRegisterRequest extends Request {
+    body: IRegisterRequestBody
+}
 
 export default {
     self: (req: Request, res: Response, next: NextFunction) => {
@@ -28,6 +34,14 @@ export default {
 
     register: (req: Request, res: Response, next: NextFunction) => {
         try {
+            const { body } = req as IRegisterRequest
+
+            const { error } = validateJoiSchema<IRegisterRequestBody>(validateRegisterBody, body)
+
+            if (error) {
+                return httpError(next, error, req, 422)
+            }
+
             httpResponse(req, res, 201, responseMessage.SUCCESS)
         } catch (err) {
             httpError(next, err, req, 500)
